5846d1720c13c2fc5b3f975705c7535da5de32a6
Chr-ss <crasche@student.codam.nl>
2025-04-09 15:08:39 +0200

    Added changes for tester to rush bash

--- test_configs.sh
+++ test_configs.sh
@@ -1,21 +1,18 @@
+#!/bin/bash
+
 RED="\033[31m"
 GRN="\033[1;32m"
 YEL="\033[33m"
 BLU="\033[34m"
-MAG="\033[35m"
-BMAG="\033[1;35m"
-CYN="\033[36m"
 BCYN="\033[1;36m"
-WHT="\033[37m"
 RESET="\033[0m"
-LINEP="\033[75G"
 
 
 path=./configs/invalid
 exe=./webserv
 configs=$(find $path -name "*" | sort)
 
-echo -e "${BLU}\tINVALID TESTER${RESET}"
+echo -e "${BCYN}\tINVALID TESTER${RESET}"
 x=0
 
 for config in $configs; do
@@ -36,7 +33,7 @@ path=./configs
 exe=./webserv
 configs=$(find "$path" -type d -name "invalid" -prune -o -type f ! -name "mime.types" -print)
 
-echo -e "${BLU}\n\t VALID TESTER${RESET}"
+echo -e "${BCYN}\n\t VALID TESTER${RESET}"
 x=0
 
 for config in $configs; do
