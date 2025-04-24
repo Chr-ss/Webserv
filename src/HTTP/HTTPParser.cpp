411afd0481bb152ada41968a454b1605f5ecdc78
Michael Moser <Michael.Moser11@web.de>
2025-04-24 18:19:21 +0200

    Delete err message

--- src/HTTP/HTTPParser.cpp
+++ src/HTTP/HTTPParser.cpp
@@ -400,7 +400,6 @@ std::string	HTTPParser::generatePath(const Config *config) {
 	for (const std::string &root : config->getRoot(result_.request_target))
 	{
 		full_path = addDir_Folder(root, "", result_.request_target);
-		std::cerr << "full path: " << full_path << std::endl;
 		bool autoindex = config->getAutoindex(full_path);
 		if (isAccessible(full_path, result_.dir_list, result_.status_code, autoindex)) {
 			return (full_path);
